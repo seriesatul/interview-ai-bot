d4fe07277af6415652a09f7a3f6ced55d38d7e6b
seriesatul <atulsingh04895@gmail.com>
2025-07-21 18:32:12 +0530

    Initial API testing

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,8 +10,8 @@ const page = () => {
     <>
     <section className='card-cta'>
       <div className='flex flex-col gap-6 max-w-lg'>
-        <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-        <p className='text-lg'>Practice on real interview questions, get instant feedback, and track your progress.</p>
+        <h2>We Don&apos;t Just Help You Prepare — We Make Sure You&apos;re Ready.</h2>
+        <p className='text-lg'>From Topic to Interview — Your <span className='text-primary-200'>PrepMate</span> Has You Covered.</p>
         <Button asChild className='btn-primary max-sm:w-full'>
           <Link href="/interview">Start an Interview</Link>
         </Button>
@@ -34,7 +34,7 @@ const page = () => {
     </section>
 
     <section className='flex flex-col gap-6 mt-8 '> 
-      <h2>Take an Interview</h2>
+      <h2>Your Courses</h2>
       <div className='w-full flex flex-row gap-4 mb-10'>
          {dummyInterviews.map((interview)=>(<InterviewCard {...interview} key={interview.id}/>))}
 
